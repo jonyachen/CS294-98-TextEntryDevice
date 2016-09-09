ae9c8d98854584fbccae6d8287d3e2b88de02c8f
Jonya Chen <jc957@cornell.edu>
2016-09-09 00:35:49 -0700

    Added in second pushbutton
    Modified code for both row and column pushbuttons w/ debouncing

--- TextEntry.ino
+++ TextEntry.ino
@@ -3,27 +3,33 @@ SYSTEM_MODE(MANUAL);
 #endif
 
 //set pin numbers (constants)
-const int ledPin = D7;      //input pin for LED
-const int buttonPin = D0;   //input pin for pushbutton
-const int enterPin = D1;    //input pinn for enter button
+const int rowPin = D0;   //input pin for row pushbutton
+const int colPin = D1;   //input pin for column pushbutton
+const int enterPin = D2;    //input pinn for enter button
 
 // Variables
-int buttonState;            //the current reading from the input pin
-int lastButtonState = HIGH; //the previous reading from the input pin
-int enterState;            //the current reading from the input pin
-int lastEnterState = HIGH; //the previous reading from the input pin
+int rowState;            //the current reading from the row input pin
+int lastRowState = HIGH; //the previous reading from the  row input pin
+int colState;            //the current reading from the  row input pin
+int lastColState = HIGH; //the previous reading from the  col input pin
+int enterState;
+int lastEnterState = HIGH;
 
 // Count of button presses
-int count = 0;
+int rowCount = 0;
+int colCount = 0;
 
-long lastDebounceButtonTime = 0;  //last time the button was pressed
-long lastDebounceEnterTime = 0;
-long debounceDelay = 50;    //debounce time - increase if the output flickers
+// Times for debouncing
+long lastDebounceRowTime = 0;   //last time the row button was pressed
+long lastDebounceColTime = 0;   //last time the col button was pressed
+long lastDebounceEnterTime = 0; //last time the enter button was pressed
+
+long debounceDelay = 50;        //debounce time - increase if the press reading flickers
 
 //to run once
 void setup() {
-  //pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(rowPin, INPUT_PULLUP);
+  pinMode(colPin, INPUT_PULLUP);
   pinMode(enterPin, INPUT_PULLUP);
 
   //sets up serial terminal
@@ -32,37 +38,61 @@ void setup() {
 
 //runs repeatedly
 void loop() {
-  int reading = digitalRead(buttonPin); //reads the state of the button
-  int enterReading = digitalRead(enterPin);
+  int rowReading = digitalRead(rowPin);     //reads the state of the row button
+  int colReading = digitalRead(colPin);     //reads the state of the col button
+  int enterReading = digitalRead(enterPin); //reads the state of the enter button
 
   //debouncing = check to see if you just pressed the button
   // (ie the input went from HIGH to LOW),  and you've waited
   // long enough since the last press to ignore any noise:
 
   //If the button state has changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    lastDebounceButtonTime = millis();     //reset the debouncing timer
+  if (rowReading != lastRowState) {
+    lastDebounceRowTime = millis();     //reset the debouncing timer for row button
+  }
+
+  if (colReading != lastColState) {
+    lastDebounceColTime = millis();     //reset the debouncing timer for column button
   }
   
   if (enterReading != lastEnterState) {
-    lastDebounceEnterTime = millis();
+    lastDebounceEnterTime = millis();  //reset the debouncing timer for enter button
   }
 
-  if ((millis() - lastDebounceButtonTime) > debounceDelay) {
+  //debounce loop for row button
+  if ((millis() - lastDebounceRowTime) > debounceDelay) {
     // whatever the reading is at, it's been there for longer
     // than the debounce delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
+    if (rowReading != rowState) {
+      rowState = rowReading;
 
-      // if new button state is LOW, increment the count for button presses
-      if (buttonState == LOW) {
-        count++;
+      // if new button state is LOW, increment the count for row button presses
+      if (rowState == LOW) {
+        rowCount++;
       }
     }
   }
 
+  
+  //debounce loop for col button
+  if ((millis() - lastDebounceColTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (colReading != colState) {
+      colState = colReading;
+
+      // if new button state is LOW, increment the count for col button presses
+      if (colState == LOW) {
+        colCount++;
+      }
+    }
+  }
+
+    //debounce loop for enter button
     if ((millis() - lastDebounceEnterTime) > debounceDelay) {
 
     // if the button state has changed:
@@ -71,13 +101,15 @@ void loop() {
 
       // if enter button is pressed
       if (enterState == LOW) {
-        Serial.println(count); //print count of button pressed
+        Serial.println(rowCount); //print count of row button pressed
+        Serial.println(colCount); //print count of col button pressed
       }
     }
   }
 
   // save the reading.  Next time through the loop,
   // it'll be the lastButtonState:
-  lastButtonState = reading;
+  lastRowState = rowReading;
+  lastColState = colReading;
   lastEnterState = enterReading;
 }
